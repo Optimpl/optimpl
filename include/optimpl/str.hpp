ec23af672f13cf851c007bf2f5506aad5c04cc56
mdgaziur <mdgaziurrahmannoor@gmail.com>
2021-02-02 13:20:53 +0600

    Do shady stuff :/

--- include/optimpl/str.hpp
+++ include/optimpl/str.hpp
@@ -57,7 +57,7 @@ optimpl::str optimpl::str::operator+(const char *rval) {
         resultString[j] = rval[abs(currentLength-j)];
     }
     resultString[totalLength + 1] = '\0';
-    optimpl::str result = resultString;
+    static optimpl::str result = resultString;
     delete [] resultString;
     return result;
 }
