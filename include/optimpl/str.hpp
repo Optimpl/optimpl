92a9789bbef4c963956360a93aad407a21db4e92
mdgaziur <mdgaziurrahmannoor@gmail.com>
2021-02-02 14:03:43 +0600

    "Spaghetti Code"

--- include/optimpl/str.hpp
+++ include/optimpl/str.hpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "internal/logr.hpp"
 #include "internal/str.hpp"
 
@@ -5,81 +6,31 @@ namespace optimpl
 {
     class str
     {
+
     private:
-        char *c_string = nullptr;
-        void freeit();
+        char *c_string;
+        unsigned int size;
 
     public:
-        str(const char *source);
-        str();
-        str(const str&);
+        str(const char *);
+        str(const str &);
         ~str();
-        int __len__();
-        operator const char *();
-        optimpl::str operator+(const char *);
-        void operator+=(const char *);
-        bool operator==(const char *);
+
+        char &at(unsigned int);
+        void reverse();
+        char *c_str();
+        void capitalize();
+        // list split(const char*);
+
+        unsigned int __len__();
+
+        char &operator[](unsigned int);
+        str &operator+=(const char *);
+        str &operator+=(const str &);
+        str &operator+(const char *);
+        str &operator+(const str &);
+        str &operator*(unsigned int);
+
+        friend std::ostream &operator<<(std::ostream &, const str &);
     };
 } // namespace optimpl
-
-optimpl::str::str(const char *source)
-{
-    int len = strlen(source);
-    this->c_string = new char[len+1];
-    memmove(this->c_string, source, len);
-    this->c_string[len] = '\0';
-}
-optimpl::str::str()
-{
-    this->c_string = new char[0];
-}
-optimpl::str::str(const optimpl::str &str) {
-    this->c_string = str.c_string;
-}
-optimpl::str::~str()
-{
-    delete[] this->c_string;
-}
-int optimpl::str::__len__()
-{
-    return strlen(this->c_string);
-}
-optimpl::str::operator const char *()
-{
-    return this->c_string;
-}
-optimpl::str optimpl::str::operator+(const char *rval) {
-    int rvalLength = strlen(rval);
-    int currentLength = strlen(this->c_string);
-    int totalLength = rvalLength + currentLength;
-    char* resultString = new char[totalLength + 1];
-
-    for(int i = 0; i < currentLength; i++) {
-        resultString[i] = this->c_string[i];
-    }
-    for(int j = currentLength; j < totalLength; j++) {
-        resultString[j] = rval[abs(currentLength-j)];
-    }
-    resultString[totalLength + 1] = '\0';
-    optimpl::str result = resultString;
-    return result;
-}
-void optimpl::str::operator+=(const char *rval) {
-    int rvalLength = strlen(rval);
-    int currentLength = strlen(this->c_string);
-    int totalLength = rvalLength + currentLength;
-    char* resultString = new char[totalLength + 1];
-
-    for(int i = 0; i < currentLength; i++) {
-        resultString[i] = this->c_string[i];
-    }
-    for(int j = currentLength; j < totalLength; j++) {
-        resultString[j] = rval[abs(currentLength-j)];
-    }
-    resultString[totalLength + 1] = '\0';
-    this->c_string = resultString;
-}
-bool optimpl::str::operator==(const char *rval)
-{
-    return strcmp(this->c_string, rval) == 0;
-}
