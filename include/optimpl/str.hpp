6dea82c665754f087f5ae507299bad61759d8f0f
mdgaziur <mdgaziurrahmannoor@gmail.com>
2021-02-02 16:22:41 +0600

    Implement some methods for str class

--- include/optimpl/str.hpp
+++ include/optimpl/str.hpp
@@ -18,11 +18,11 @@ namespace optimpl
 
         char &at(unsigned int);
         void reverse();
-        char *c_str();
+        const char *c_str();
         void capitalize();
         // list split(const char*);
 
-        unsigned int __len__();
+        unsigned int __len__() const;
 
         char &operator[](unsigned int);
         str &operator+=(const char *);
@@ -30,8 +30,90 @@ namespace optimpl
         str &operator+(const char *);
         str &operator+(const str &);
         str &operator*(unsigned int);
-
-        friend std::ostream &operator<<(std::ostream &, const str &);
-        friend std::istream &operator>>(std::istream &, const str &);
+        bool operator==(const char *);
+        bool operator==(const str &);
     };
 } // namespace optimpl
+
+optimpl::str::str(const char *source)
+{
+    this->size = strlen(source);
+    this->c_string = new char[this->size + 1];
+    memcpy(c_string, source, this->size);
+}
+
+optimpl::str::str(const str &source)
+    : size(source.size)
+{
+    this->c_string = new char[this->size];
+    memcpy(this->c_string, source.c_string, this->size);
+}
+
+optimpl::str::~str()
+{
+    delete[] this->c_string;
+}
+
+char &optimpl::str::at(unsigned int idx)
+{
+    if (idx < 0 || idx > this->size - 1)
+    {
+        throw "OutOfRangeException: Index is out of range";
+    }
+    return this->c_string[idx];
+}
+
+void optimpl::str::reverse()
+{
+    for (unsigned int i = 0, j = this->size - 1; i < (this->size / 2); i++, j--)
+    {
+        char temp = this->c_string[i];
+        this->c_string[i] = this->c_string[j];
+        this->c_string[j] = temp;
+    }
+}
+
+/*
+    This function returns pointer to the core c_string variable which contains the string.
+    Editing that may cause problems.
+*/
+const char *optimpl::str::c_str()
+{
+    return this->c_string;
+}
+
+void optimpl::str::capitalize()
+{
+    bool isspace = false;
+    for (unsigned int i = 0; i < this->size; i++)
+    {
+        if (this->c_string[i] == ' ')
+        {
+            isspace = true;
+        }
+        else if (isspace)
+        {
+            this->c_string[i] = toupper(this->c_string[i]);
+            isspace = false;
+        }
+        else
+        {
+            this->c_string[i] = tolower(this->c_string[i]);
+        }
+    }
+}
+
+unsigned int optimpl::str::__len__() const
+{
+    return this->size;
+}
+
+bool optimpl::str::operator==(const char *rval)
+{
+    return strcmp(this->c_string, rval) == 0;
+}
+
+bool optimpl::str::operator==(const str& rval)
+{
+    return strcmp(this->c_string, rval.c_string) == 0;
+}
--- tests/teststr.cpp
+++ tests/teststr.cpp
@@ -7,17 +7,7 @@
 
 void teststr()
 {
-    optimpl::str s = "Test String";
-    assert(s == "Test String");
-    // assert(s == std::string("Test String"));
-    assert((s + " Addition") == "Test String Addition");
-    assert(optimpl::len(s + " Addition") == 20);
-    // assert((s + std::string(" Addition")) == std::string("Test String Addition"));
-    // assert(len(s + std::string(" Addition"))== 20);
-    s += " Permanent Addition";
-    assert(s == "Test String Permanent Addition");
-    assert(optimpl::len(s)==30);
-    // s += std::string(" std::string()");
-    // assert(s == "Test String Permanent Addition std::string()");
-    // assert(optimple::len(s)==44);
+    optimpl::str string = "Hello World!";
+    assert(string == "Hello World!");
+    string.reverse();
 }
